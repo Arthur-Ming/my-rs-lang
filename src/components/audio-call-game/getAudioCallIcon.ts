f15eddea26e1dd56582e68346157006301d1c56d
Artur Mingazhov <arthurming7@gmail.com>
2022-02-20 18:14:35 +0300

    deploy

--- src/components/audio-call-game/getAudioCallIcon.ts
+++
@@ -1,9 +0,0 @@
-export default function () {
-  return `
-<div class="audio-call__call"  data-audio-call-voice>
-  <svg class=" audio-call__call-icon" viewBox="0 0 128 128">
-    <use xlink:href='assets/svg/sprite.svg#audio' />
-  </svg>
-</div>
-  `
-}
--- src/utils/withLouding.ts
+++
@@ -1,6 +0,0 @@
-export default async function <T>(proms: T): Promise<T> {
-  document.body.classList.add('_spinner')
-  const res = await proms
-  document.body.classList.remove('_spinner')
-  return res
-}
